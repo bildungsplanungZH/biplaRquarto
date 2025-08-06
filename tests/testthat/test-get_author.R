16cd95b576174d1a795e6be8e8b7fc4c13676dea
Sarah Gerhard <sarah.gerhard@bi.zh.ch>
2025-08-06 10:59:27 +0200

    test with `get_author(test_path)`

--- tests/testthat/test-get_author.R
+++ tests/testthat/test-get_author.R
@@ -1,18 +1,18 @@
 test_that("returns a list with elements named user and org", {
-    expect_equal(class(get_author()), "list")
-    expect_named(get_author(), c("user", "org"))
+    expect_equal(class(get_author(path = test_path())), "list")
+    expect_named(get_author(path = test_path()), c("user", "org"))
 
 })
 
 test_that("element user has elements named family, given and email", {
-    expect_equal(class(get_author()$user), "list")
-    expect_contains(names(get_author()$user), c("family", "given", "email"))
+    expect_equal(class(get_author(path = test_path())$user), "list")
+    expect_contains(names(get_author(path = test_path())$user), c("family", "given", "email"))
 
 })
 
 test_that("element org has elements named dir and org1", {
-    expect_equal(class(get_author()$org), "list")
-    expect_contains(names(get_author()$org), c("dir", "org1"))
+    expect_equal(class(get_author(path = test_path())$org), "list")
+    expect_contains(names(get_author(path = test_path())$org), c("dir", "org1"))
 
 })
 
