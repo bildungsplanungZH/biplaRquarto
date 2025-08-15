20d1db193acf0b0007942b304bcc71ecad6c60e6
sageorca <sageorca@users.noreply.github.com>
2025-08-15 21:06:55 +0000

    Style code (GHA)

--- R/use_quarto.R
+++ R/use_quarto.R
@@ -104,33 +104,30 @@ use_quarto <- function(file_name = "report", ext_name = "biplaR-html",
   path_prefix <- paste0(rep("../", depth), collapse = "")
 
   if (depth > 0) {
+    readLines(paste0(file_name, ".qmd", collapse = "")) |>
+      gsub(
+        pattern = "_extensions",
+        replacement = paste0(path_prefix, "_extensions"), x = _
+      ) |>
+      writeLines(con = paste0(file_name, ".qmd"))
 
-      readLines(paste0(file_name, ".qmd", collapse = "")) |>
+    if (ext_name %in% c("biplaR-html", "biplaR-revealjs")) {
+      readLines(paste0("_extensions/", ext_name, "/partials/include-header.html")) |>
+        gsub(
+          pattern = "_extensions",
+          replacement = paste0(path_prefix, "_extensions"), x = _
+        ) |>
+        writeLines(con = paste0("_extensions/", ext_name, "/partials/include-header.html"))
+
+      if (ext_name == "biplaR-revealjs") {
+        readLines(paste0("_extensions/", ext_name, "/partials/title-slide.html")) |>
           gsub(
-              pattern = "_extensions",
-              replacement = paste0(path_prefix, "_extensions"), x = _
+            pattern = "_extensions",
+            replacement = paste0(path_prefix, "_extensions"), x = _
           ) |>
-          writeLines(con = paste0(file_name, ".qmd"))
-
-      if (ext_name %in% c("biplaR-html", "biplaR-revealjs")) {
-
-          readLines(paste0("_extensions/", ext_name, "/partials/include-header.html")) |>
-              gsub(
-                  pattern = "_extensions",
-                  replacement = paste0(path_prefix, "_extensions"), x = _
-              ) |>
-              writeLines(con = paste0("_extensions/", ext_name, "/partials/include-header.html"))
-
-          if (ext_name == "biplaR-revealjs") {
-
-              readLines(paste0("_extensions/", ext_name, "/partials/title-slide.html")) |>
-                  gsub(
-                      pattern = "_extensions",
-                      replacement = paste0(path_prefix, "_extensions"), x = _
-                  ) |>
-                  writeLines(con = paste0("_extensions/", ext_name, "/partials/title-slide.html"))
-          }
+          writeLines(con = paste0("_extensions/", ext_name, "/partials/title-slide.html"))
       }
+    }
   }
 
   # open the new file in the editor
