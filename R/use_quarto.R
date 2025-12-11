1f89bec3404ce66590a11a45a5ff2e5eae67f3f8
sageorca <sageorca@users.noreply.github.com>
2025-12-11 10:01:05 +0000

    Style code (GHA)

--- R/use_quarto.R
+++ R/use_quarto.R
@@ -40,7 +40,7 @@ use_quarto <- function(file_name = "report", ext_name = "biplaR-html",
   }
 
   if (ext_name == "biplaR-revealjs" &&
-        bg_image != "_extensions/biplaR-revealjs/images/panorama.png") {
+    bg_image != "_extensions/biplaR-revealjs/images/panorama.png") {
     if (!file.exists(bg_image)) {
       stop("Background image not found at ", bg_image)
     }
@@ -116,7 +116,7 @@ use_quarto <- function(file_name = "report", ext_name = "biplaR-html",
     writeLines(con = paste0(file_name, ".qmd"))
 
   if (ext_name == "biplaR-revealjs" &&
-        bg_image != "_extensions/biplaR-revealjs/images/panorama.png") {
+    bg_image != "_extensions/biplaR-revealjs/images/panorama.png") {
     readLines(paste0(file_name, ".qmd", collapse = "")) |>
       gsub(
         pattern = "_extensions/biplaR-revealjs/images/panorama.png",
